2b60b3e342ad932f37cba8fad4d8d8a3c9be4192
cms <audtlr93@naver.com>
2022-10-09 23:14:10 +0900

    ...

--- database/database.js
+++ database/database.js
@@ -6,8 +6,8 @@ let database;
 
 let mongodbUrl = "mongodb://127.0.0.1:27017"
 
-if(process.env.MONGODB_URL){
-    mongodbUrl = process.env.MONGODB_URL
+if(process.env.MONGODB_URI){
+    mongodbUrl = process.env.MONGODB_URI
 }
 
 async function connectToDatabse(){
