9a9cd122113a3e1a2bf765d6e583718115763a69
cms <audtlr93@naver.com>
2022-10-10 14:37:32 +0900

    delete stripte

--- controllers/pay-to-order-Controller.js
+++ controllers/pay-to-order-Controller.js
@@ -1,6 +1,6 @@
 const PayOrder = require("../models/pay-to-order-model")
 const CartModel = require("../models/cart-model")
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const stripe = require('stripe')('sk_test_51LpNaFKewLSBpof5bSklhnQxWtZdAABciZHwH6JlPatHR7h4XNIj7Oi7g2Eldc2xX6Z9zLzCyK3P1BvzYEXVzTg600RRSGJq8M');
 
 async function getOrder(req,res){
 
@@ -10,81 +10,81 @@ async function getOrder(req,res){
 }
 
 
-async function saveInAdminOrder(req,res){
-    // orderData는 그대로니까, 주문번호만 추가해서 넘기고
-    // userData는 주문자 데이터니까 바뀔수 있으니 req.body로
+// async function saveInAdminOrder(req,res){
+//     // orderData는 그대로니까, 주문번호만 추가해서 넘기고
+//     // userData는 주문자 데이터니까 바뀔수 있으니 req.body로
 
-    let bd = req.body;
+//     let bd = req.body;
 
-    const Data = await CartModel.orderData(res.locals.uid)
+//     const Data = await CartModel.orderData(res.locals.uid)
     
-    const productData = {
-        orderId : bd.orderId.toString(),
-        ...Data
-    }
+//     const productData = {
+//         orderId : bd.orderId.toString(),
+//         ...Data
+//     }
 
-    const CartDatas = await CartModel.findAllproductsInCart(res.locals.uid)
+//     const CartDatas = await CartModel.findAllproductsInCart(res.locals.uid)
 
-    const newData = []
+//     const newData = []
     
-    for(i=0 ;  i < CartDatas.length; i++){
-        newData.push({
-            ...CartDatas[i],
-            productQuantity : Data.productsQuantity[i]
-        })
-    }
+//     for(i=0 ;  i < CartDatas.length; i++){
+//         newData.push({
+//             ...CartDatas[i],
+//             productQuantity : Data.productsQuantity[i]
+//         })
+//     }
 
-    const userData = {
-        email : bd.email,
-        name: bd.username,
-        address:{
-            street: bd.street,
-            postal: bd.postal,
-            country: bd.country
-        }
-    }
+//     const userData = {
+//         email : bd.email,
+//         name: bd.username,
+//         address:{
+//             street: bd.street,
+//             postal: bd.postal,
+//             country: bd.country
+//         }
+//     }
 
-    // Stripe API를 사용한, 결제시스템 구축
+//     // Stripe API를 사용한, 결제시스템 구축
 
-    // 4242 4242 4242 4242 카드번호
+//     // 4242 4242 4242 4242 카드번호
 
-    // 오류1. line_items : [ name:1 price:1 quantity:1] ... 
-    //        line_itmes : [ name :13 , price: 13, quantity: 13...] 
-    //          이런 식으로  line_items가 [각 하나의 값마다] 들어가야 하기 때문에
+//     // 오류1. line_items : [ name:1 price:1 quantity:1] ... 
+//     //        line_itmes : [ name :13 , price: 13, quantity: 13...] 
+//     //          이런 식으로  line_items가 [각 하나의 값마다] 들어가야 하기 때문에
 
-    //      ==> 들어가는 값은 반드시, 배열 [ name : x, price:y ,quantity : 54] ... [name : vc, price: jk , quantity: 23] ... 처럼 들어가야한다.
-    //      ==>> 나는 { name : [ x, y, z], {price : [x, y, z]} ... 식이기 때문에 } 실패
+//     //      ==> 들어가는 값은 반드시, 배열 [ name : x, price:y ,quantity : 54] ... [name : vc, price: jk , quantity: 23] ... 처럼 들어가야한다.
+//     //      ==>> 나는 { name : [ x, y, z], {price : [x, y, z]} ... 식이기 때문에 } 실패
 
-    //      ==> 그렇다면 값을 [order collection이 아니라 ] [ Cart collection 에서 각각 있는 값을 가져오면 되겠다]
-    //          단, 내 Cart collection에는 "수량" 이 없으니까 추가하도록하자!
+//     //      ==> 그렇다면 값을 [order collection이 아니라 ] [ Cart collection 에서 각각 있는 값을 가져오면 되겠다]
+//     //          단, 내 Cart collection에는 "수량" 이 없으니까 추가하도록하자!
 
-    const pay = new PayOrder(productData,userData,"pending","now")
-    req.session.ADMINORDER = pay
+//     const pay = new PayOrder(productData,userData,"pending","now")
+//     req.session.ADMINORDER = pay
 
-    await PayOrder.makeEmptyOrder(res.locals.uid)
-    await CartModel.makeEmptyCart(res.locals.uid)
+//     await PayOrder.makeEmptyOrder(res.locals.uid)
+//     await CartModel.makeEmptyCart(res.locals.uid)
     
-    const session = await stripe.checkout.sessions.create({
-        line_items: newData.map(function(item){
-            return{
-                price_data : {
-                  currency : "usd",
-                  product_data : {
-                      name : item.productName
-                  },
-                  unit_amount : +item.productPrice
+//     const session = await stripe.checkout.sessions.create({
+//         line_items: newData.map(function(item){
+//             return{
+//                 price_data : {
+//                   currency : "usd",
+//                   product_data : {
+//                       name : item.productName
+//                   },
+//                   unit_amount : +item.productPrice
   
-                },
-                quantity: +item.productQuantity,
-              }
-        }),
-        mode: 'payment',
-        success_url: `/success`,
-        cancel_url: `/cancel`,
-    });
+//                 },
+//                 quantity: +item.productQuantity,
+//               }
+//         }),
+//         mode: 'payment',
+//         success_url: `http://localhost:3000/success`,
+//         cancel_url: `http://localhost:3000/cancel`,
+//     });
 
-    res.redirect(303, session.url)
-}
+//     res.redirect(303, session.url)
+// }
 
 
 async function getAllOrderForClient(req,res){
@@ -125,7 +125,7 @@ function payCancel(req,res){
 
 module.exports ={
     getOrder:getOrder,
-    saveInAdminOrder:saveInAdminOrder,
+    // saveInAdminOrder:saveInAdminOrder,
     getAllOrderForClient:getAllOrderForClient,
     getOrderDetail:getOrderDetail,
     paySuccess:paySuccess,
